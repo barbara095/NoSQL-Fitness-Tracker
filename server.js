d00587efb8c91544384db037ff0bd6eb391e1881
Barbs <barbarap095@gmail.com>
2020-09-01 21:16:42 +1000

    Fix mongoose connection

---
+++ server.js
@@ -0,0 +1,32 @@
+// Requiring Express and mongoose
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("public/models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// const routes = require("./routes/api.js");
+
+// app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 
+    "mongodb://localhost/fitness", 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
