087b7724bdd52b3a7dcf39b2573c0bf9809ade8e
Barbs <barbarap095@gmail.com>
2020-09-03 23:26:50 +1000

    Export workout model in index file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,4 @@
-init();
-
-async function init() {
-  if (location.search.split("=")[1] === undefined) {
-    const workout = await API.getLastWorkout();
-    if (workout) {
-      location.search = "?id=" + workout._id;
-    } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
-    }
-  }
-}
-
+// Export object of our Workout model
+module.exports = {
+  Workout: require('./workouts')
+};
