ac623c63895952c527fa68102b1250f9529f1e27
Barbs <barbarap095@gmail.com>
2020-09-05 20:06:43 +1000

    Fix api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-
+// Requiring our Workout model to handle API requests
 const Workout = require("../models/workouts.js");
 
 // Routes
 // =============================================================
 // Route for getting workout data from model
-router.get("api/workouts", ({ body }, res) => {
+router.get("/api/workouts", (req, res) => {
   Workout.find(body)
   .then(dbWorkout => {
     res.json(dbWorkout);
@@ -16,7 +16,7 @@ router.get("api/workouts", ({ body }, res) => {
 });
 
 // Route for posting workout data 
-router.post("api/workouts", ({ body }, res) => {
+router.post("/api/workouts", ( { body }, res) => {
   Workout.create(body)
   .then(dbWorkout => {
     res.json(dbWorkout);
@@ -27,11 +27,11 @@ router.post("api/workouts", ({ body }, res) => {
 });
 
 // Route for updating workout data 
-router.put("api/workouts/:id", (req, res) => {
+router.put("/api/workouts/:id", (req, res) => {
   Workout.findOneAndUpdate ({
     _id: req.params.id
   }, {
-    $push: {exercises: req.body}
+    $push: { exercises: req.body }
   })
   .then(dbWorkout => {
     res.json(dbWorkout);
@@ -43,7 +43,7 @@ router.put("api/workouts/:id", (req, res) => {
 });
 
 // Route for getting workouts in range
-router.get("api/workouts/range", ({ body }, res) => {
+router.get("/api/workouts/range", ({ body }, res) => {
   Workout.find(body)
   .then(dbWorkout => {
     res.json(dbWorkout);
