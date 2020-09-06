535b5c652863add7bddfb42ce15259f856fcac44
Barbs <barbarap095@gmail.com>
2020-09-06 21:46:20 +1000

    Add total duration property to schema

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # NoSQL-Fitness-Tracker
-An application that uses MongoDB to enable a user to view, create and keep track of their workouts. 
+An application that enables a user to view, create and keep track of their workouts. Data is stored as a collection in a Mongo database. 
 
 ## User Story
 
  As a user, I want to be able to view, create and track daily workouts. I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
+ I also want to see a visual summary of my workout statistics, including the combined weight of multiple exercises in the stats page. 
+
+ 
--- models/workouts.js
+++ models/workouts.js
@@ -2,45 +2,59 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  exercises: {
-    type: {
-      type: String,
-      trim: true,
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
     },
-    name: {
-      type: String,
-      trim: true,
-    },
-    duration: {
-      type: Number,
-      trim: true,
-    },
-    distance: {
-      type: Number,
-      trim: true,
-    },
-    weight: {
-      type: Number,
-      trim: true,
-    },
-    reps: {
-      type: Number,
-      trim: true,
-    },
-    sets: {
-      type: Number,
-      trim: true,
-    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+        },
+        duration: {
+          type: Number,
+          trim: true,
+        },
+        distance: {
+          type: Number,
+          trim: true,
+        },
+        weight: {
+          type: Number,
+          trim: true,
+        },
+        reps: {
+          type: Number,
+          trim: true,
+        },
+        sets: {
+          type: Number,
+          trim: true,
+        },
+      }
+    ],
   },
-  
-  day: {
-    type: Date,
-    default: Date.now
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 
 });
-
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
